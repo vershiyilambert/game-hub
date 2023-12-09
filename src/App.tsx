e25af9158c2e54b240bc2a7e481fb0749aae27d6
Vershiyi Lambert <vershiyilambert@yahoo.com>
2023-12-09 10:31:43 -0800

    Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-function App() {
-  return (
-    <>
-      <h1> Game Hub</h1>
-    </>
-  );
-}
+import { Button } from "@chakra-ui/react";
+
+const App = () => {
+  return <Button colorScheme="blue">Click Me</Button>;
+};
 
 export default App;
